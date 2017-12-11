34004973014b66100a1dc6742ec142f4a7f36168
spinelli <spinelli@gmail.com>
2017-12-11 18:27:55 -0300

    Repositorio Local Jdeveloper

---
+++ Client/src/main/java/CalcMatrices/Interprete.java
@@ -0,0 +1,11 @@
+package CalcMatrices;
+
+public class Interprete {
+    public Interprete() {
+        super();
+    }
+
+    public static void main(String[] args) {
+        Interprete interprete = new Interprete();
+    }
+}
